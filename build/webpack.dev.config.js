308e82689c50223f546a080fb55b0ac6f6cdff62
tjx <taojiaxiang_work@163.com>
2020-10-14 17:28:25 +0800

    首次提交

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    devtool: 'cheap-module-eval-source-map'     // module 表示使用的是typescript 而不是 经过loader转译过后的JS
+}
--- greeter.js
+++
@@ -1,5 +0,0 @@
-function add(x, y) {
-    return y;
-}
-var add1 = add;
-console.log(add1(1, '3'));
